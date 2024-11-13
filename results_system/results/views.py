80dad487427ee2b7776a86ac58bf0bead0422e41
poni-henry <ponihenry88@gmail.com>
2024-11-13 11:38:09 +0200

    created a view to display sudents results and CGPA

--- results_system/results/views.py
+++ results_system/results/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Student
 
-# Create your views here.
+def student_results(request, student_id):
+    student = get_object_or_404(Student, student_id=student_id)
+    results = student.result_set.all()
+    cgpa = student.calculate_cgpa()
+    return render(request, 'results/student_results.html', {'student': student, 'results': results, 'cgpa': cgpa})
