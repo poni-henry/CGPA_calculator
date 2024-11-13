017d6f074c637787527a1ec5fb63ce6521a08a3b
poni-henry <ponihenry88@gmail.com>
2024-11-13 12:06:51 +0200

    added a url to students results view

--- results_system/results_system/urls.py
+++ results_system/results_system/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('student/<str:student_id>/', views.student_results, name='student_results'),
 ]
